d2a1aad809063566d0c25d1b16bc22815f591f4c
Camilo Rodriguez Cuaran <camiepisode@outlook.com>
2024-10-03 18:30:29 -0500

    Fix: Included missing reference

--- src/hooks/useAuth.hook.jsx
+++ src/hooks/useAuth.hook.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useMemo } from 'react';
+import React, { createContext, useContext, useMemo, useEffect } from 'react';
 import { useLocalStorage } from './useLocalStorage.hook';
 
 /**
