97b50dbf2e47f6e64087777a1920d1b695e59b75
Camilo Rodriguez Cuaran <camiepisode@outlook.com>
2024-10-10 18:57:35 -0500

    Feat: Exported Verify components

--- src/index.js
+++ src/index.js
@@ -20,6 +20,12 @@ export { VeripassQuickUserBiometricsFingerprint as VeripassQuickUserBiometricsFi
 export { VeripassQuickUserBiometricsVoice as VeripassQuickUserBiometricsVoice } from './components/quick-actions/biometrics/VeripassQuickUserBiometricsVoice.jsx';
 export { VeripassQuickUserBiometricsSignature as UserQuickBiometricsSignature } from './components/quick-actions/biometrics/VeripassQuickUserBiometricsSignature.jsx';
 
+/* Verify */
+export { VeripassUserNotVerifiedBanner as VeripassUserVerifyButton } from './components/verify/VeripassUserNotVerifiedBanner';
+export { VeripassUserVerificationStatus as VeripassUserVerifyButton } from './components/verify/VeripassUserVerificationStatus';
+export { VeripassUserVerifiedBanner as VeripassUserVerifyButton } from './components/verify/VeripassUserVerifiedBanner';
+export { VeripassUserVerifyButton as VeripassUserVerifyButton } from './components/verify/VeripassUserVerifyButton';
+
 export {
   AuthContext,
   AuthProvider,
