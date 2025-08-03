41cd603e2a5696d80534da1596b827cbcc61011b
Camilo Rodriguez Cuaran <camiepisode@outlook.com>
2025-08-03 17:09:09 -0500

    Feat: Add Storybook story for VeripassIdentityContractList
    Introduces a Storybook story for the VeripassIdentityContractList component, including controls for organization, redirectUrl, environment, and apiKey. The story is wrapped with MemoryRouter and AuthProvider for context.

---
+++ src/stories/identity/identity-contract/VeripassIdentityContractList.stories.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { VeripassIdentityContractList } from '../../../components/identity/identity-contract/list/VeripassIdentityContractList';
+import { AuthProvider } from '../../../hooks/useAuth.hook';
+
+export default {
+  title: 'Identity/Contract/VeripassIdentityContractList',
+  component: VeripassIdentityContractList,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    organization: { control: 'object' },
+    redirectUrl: { control: 'text' },
+    environment: { control: 'string' },
+    apiKey: { control: 'text' },
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ width: '100%' }}>
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+const Template = (args) => (
+  <MemoryRouter>
+    <AuthProvider {...args}>
+      <VeripassIdentityContractList {...args} />
+    </AuthProvider>
+  </MemoryRouter>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  ui: {
+    profilePhoto: {
+      height: '95',
+    },
+    inputSize: 'small',
+  },
+  environment: 'development',
+  apiKey: '',
+  debug: true,
+};
